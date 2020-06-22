eef0577f462e0b418212060481af09a440a5704d
boini-raesh408 <rameshboini008@gmail.com>
2020-06-22 07:21:04 +0530

    collaborator dialog function created

--- src/app/edit-collaborator/edit-collaborator.component.ts
+++ src/app/edit-collaborator/edit-collaborator.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { NotesServiceService } from '../services/notesService/notes-service.service';
 
 @Component({
   selector: 'app-edit-collaborator',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-collaborator.component.scss']
 })
 export class EditCollaboratorComponent implements OnInit {
-
-  constructor() { }
+  @Output() sendCollaborators = new EventEmitter(false)
+  listOfUsers
+  constructor(private dialog: MatDialog, private ns : NotesServiceService) { }
 
   ngOnInit() {
   }
 
+  openCollaboratorDialog(): void{
+    const dialogRef = this.dialog.open(CollabDialgBoxComponent, {
+      width: "40rem",
+      // height: "2rem",
+      data: this.listOfUsers,
+      panelClass: 'dialog-container'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.listOfUsers = result;
+      console.log(this.listOfUsers,"list")
+      // this.ns.collabSource.next(this.listOfUsers)
+      this.sendCollaborators.emit(this.listOfUsers);
+      console.log("Collaborators DialogBox Closed!");
+    })
+  } 
+  setCollaborator($event){
+    console.log($event,"cols")
+  }
 }
